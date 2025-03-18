d3ee611791e51c6343c73574735f737591132f60
bhagyashriirdande <bhagyashriirdande2004@gmail.com>
2025-03-18 10:00:56 +0530

    Update Task.js

--- todo-app/src/component/Task.js
+++ todo-app/src/component/Task.js
@@ -1,1 +1,12 @@
+import { Check, Trash } from "lucide-react";
+
+export default function Task({ task, index, toggleComplete, deleteTask }) {
+  return (
+    <div className="task">
+      <span>{index + 1}. {task.text}</span>
+      <button onClick={() => toggleComplete(index)}><Check /></button>
+      <button onClick={() => deleteTask(index)}><Trash /></button>
+    </div>
+  );
+}
 
