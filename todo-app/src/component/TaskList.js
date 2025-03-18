19864cc8bc8ee47e2a664143e116a9ae04cd4c89
bhagyashriirdande <bhagyashriirdande2004@gmail.com>
2025-03-18 10:01:59 +0530

    Update TaskList.js

--- todo-app/src/component/TaskList.js
+++ todo-app/src/component/TaskList.js
@@ -1,1 +1,23 @@
+import Task from "./Task";
+
+export default function TaskList({ tasks, setTasks }) {
+  const toggleComplete = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div>
+      {tasks.map((task, index) => (
+        <Task key={index} task={task} index={index} toggleComplete={toggleComplete} deleteTask={deleteTask} />
+      ))}
+    </div>
+  );
+}
 
